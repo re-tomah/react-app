d75f76d4f448b7de3b369bc2296b2a790fc12540
re-tomah <pianistas2601@gmail.com>
2020-10-27 08:44:59 +0200

    Updates

---
+++ precache-manifest.f0d9ae2b0b51111e6e166dabb927be7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61687a9b0b0159ad7d1aeb0d8c8c7886",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "d93a1219aaf111b84878",
+    "url": "/my-app/static/css/main.b3e888fe.chunk.css"
+  },
+  {
+    "revision": "23f7dff2b6378c407c1d",
+    "url": "/my-app/static/js/2.0fa57018.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/my-app/static/js/2.0fa57018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d93a1219aaf111b84878",
+    "url": "/my-app/static/js/main.24fcf87c.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
